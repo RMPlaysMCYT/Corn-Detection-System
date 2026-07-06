f07c82509aea58716e583fe33eb5864da10b2462
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-07-06 10:35:40 +0800

    add python file

--- python/corn_seed_model_v1.py
+++ python/corn_seed_model_v1.py
@@ -1,1 +1,11 @@
-import
+import tensorflow as tf 
+from tensorflow.keras import layers, models, callbacks
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import pandas as pd
+import os
+import random
+import time
+import shutil
+
+from tensorflow.keras import mixed_precision
