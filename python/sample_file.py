b284940346084780213786f0f85591495f435022
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-07-06 10:43:53 +0800

    add sample file to note tho

---
+++ python/sample_file.py
@@ -0,0 +1,12 @@
+#### This is a sample file to test my neovim skill at least, go F the purist/haters, pick a tool to perform well
+#### This must not be Neovim vs VSCode or any as long as it delivers the job
+
+
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
