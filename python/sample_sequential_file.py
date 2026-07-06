4818e87a0abb5dee3d0cd0bb081fac0609b45cb0
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-07-06 11:00:17 +0800

    adding few

--- python/sample_sequential_file.py
+++ python/sample_sequential_file.py
@@ -7,4 +7,19 @@ model = Sequential([
         activation='relu',
         input_shape=(32,32,3)
     ),
+
+    MaxPooling2D((2,2)),
+    Conv2D(
+        64,(3,3),
+        activation='relu'
+    ),
+
+    MaxPooling2D((2,2)),
+    Conv2D(128,(3,3),activation='relu'),
+    Flatten(),
+    Dense(256, activation='relu'),
+    Dropout(0.5),
+    Dense(10,activation='softmax')
 ])
+
+
