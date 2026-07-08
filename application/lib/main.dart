b50cc4ec837604e14776aa4406d8b94293bcaec9
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-07-08 12:13:24 +0800

    Add several changes especially on Window Adjustment and Exit Application

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,8 +1,24 @@
 import 'dart:async';
 import 'package:application/screens/_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+   windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(780, 500),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
--- application/lib/screens/settings_screen.dart
+++ application/lib/screens/settings_screen.dart
@@ -1,8 +1,23 @@
-import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
+
+
+  // Add Keys anyway:
+
+  void _exitApplication() {
+    if (Platform.isAndroid) {
+      SystemNavigator.pop(); 
+    } else if (Platform.isIOS) {
+      exit(0); 
+    } else {
+      exit(0);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +53,19 @@ class SettingsScreen extends StatelessWidget {
                     "Developed by Group 5, in the Compliance for Capstone Project of 'Design and Development of IoT-Enabled Corn Zea Mays Seed Quality Segregation Prototype Using Convolutional Neural Network",
                     textAlign: TextAlign.start,
                   ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    fixedSize: const Size(200, 50), // Width: 200, Height: 50
+                  ),
+                  onPressed: () {
+                    _exitApplication();
+                  },
+                  child: const Text('Exit Application'),
                 ),
               ],
             ),
