cbfb0849bc0cd0b3e9d277015170ff27e1bacfb6
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-07-13 12:10:42 +0800

    add text lines

--- application/lib/utilities/texts.dart
+++ application/lib/utilities/texts.dart
@@ -1,2 +1,9 @@
 const HistoryText = "History";
 const DashboardText = "Dashboard";
+
+const TotalCornDetectedText = "Total Corn Detected";
+const TotalCornPassedText = "Total Corn Passed";
+const TotalCornDefectedText = "Total Corn Defected";
+
+const ReportsPage = "Reports";
+const SettingsPage = "Settings";
