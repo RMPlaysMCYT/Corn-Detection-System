caf3b19fff1d2ca3ebb8fa68d35ac6a9df062c9d
RMPlaysMCYT <ronnel.mitra.77@gmail.com>
2026-08-07 14:02:43 +0800

    add fixing the alignment

--- application/lib/screens/sorting_screen.dart
+++ application/lib/screens/sorting_screen.dart
@@ -12,7 +12,35 @@ class _SortingScreenState extends State<SortingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Sorting Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text('Status:', style: TextStyle(fontSize: 24)),
+                    Text('Current: ', style: TextStyle(fontSize: 24)),
+                    Text('Healthy: ', style: TextStyle(fontSize: 24)),
+                    Text('Unhealthy: ', style: TextStyle(fontSize: 24)),
+                    Text('System Status: ', style: TextStyle(fontSize: 24)),
+                  ],
+                ),
+                
+                Column(
+                  children: [
+                    Text('Status:', style: TextStyle(fontSize: 24)),
+                    Text('Current: ', style: TextStyle(fontSize: 24)),
+                    Text('Healthy: ', style: TextStyle(fontSize: 24)),
+                    Text('Unhealthy: ', style: TextStyle(fontSize: 24)),
+                    Text('System Status: ', style: TextStyle(fontSize: 24)),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
